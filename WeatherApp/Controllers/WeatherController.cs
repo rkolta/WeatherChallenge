caea24ef3fd3e5851f7e90e3d40feb0906f1f7f8
Rana Kolta <rkolta@kloveair1.com>
2021-02-26 15:33:50 -0800

    initial commit

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.Services;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly WeatherService _service;
+        public WeatherController(WeatherService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            Weather model= await _service.GetWeatherAsync();
+            return View(model);
+        }
+    }
+}
---
+++ WeatherApp/Services/WeatherService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _client;
+        private readonly Uri _url;
+        public WeatherService(HttpClient client, IConfiguration config)
+        {
+            _client = client;
+            _url = new Uri(config.GetValue<string>("WeatherUrl"));
+        }
+
+        public async Task<Weather> GetWeatherAsync()
+        {
+            var response =  await _client.GetAsync(_url);
+            return JsonConvert.DeserializeObject<Weather>(await response.Content.ReadAsStringAsync());
+
+        }
+    }
+}
